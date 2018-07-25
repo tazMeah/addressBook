e115243939c490751bd5c3391c4d57fb45e87673
tazMeah <montazmeahii@yahoo.com>
2018-07-25 18:08:51 -0400

    lab is complete version using a button

--- script.js
+++ script.js
@@ -39,53 +39,57 @@ const book = new AddressBook();
 // loop to prompt user to add, delete, print
 // or quit
 
-function start(){
-    let choice = prompt("ADD, INDEX delete, NAME delete, UPDATE, PRINT, or QUIT");
-
-    //if add
-    if (choice == "ADD") {
-        book.add(new Contact(prompt("Enter name"), prompt("Enter email"), prompt("Enter phone"), prompt("Enter relation")));
-        console.log(book);
-    }
-
-    //if delete by index
-    if (choice == "INDEX") {
-        let deleteIndex = prompt("Enter index number to delete.");
-        book.contacts.splice(deleteIndex, 1);
-        console.log(book);
-
-
-    }
- 
-
-    //if delete by name
-    if (choice == "NAME") {
-        let deleteContact = prompt("Enter name of contact to delete.");
-        //loop through contacts, searching for name match
-        for (let i = 0; i < book.contacts.length; i++) {
-            if (book.contacts[i].name == deleteContact) {
-                book.contacts.splice(i, 1);
-            }
+    function start(){ //for button
+        let choice = prompt("ADD, INDEX delete, NAME delete, UPDATE, PRINT, or QUIT");
+    //while () {
+        //if add
+        if (choice == "ADD") {
+            book.add(new Contact(prompt("Enter name"), prompt("Enter email"), prompt("Enter phone"), prompt("Enter relation")));
+            console.log(book);
         }
-        console.log(book);
-    }
+
+        //if delete by index
+        if (choice == "INDEX") {
+            let deleteIndex = prompt("Enter index number to delete.");
+            book.contacts.splice(deleteIndex, 1);
+            console.log(book);
+
+
+        }
     
-    //if update
-    if (choice == "UPDATE") {
-        let updateIndex = prompt("Enter index to update");
-        let updateProperty = prompt("Editing: NAME, EMAIL, PHONE, or RELATION?").toLowerCase();
-        let editedValue = prompt(`Enter the new value for ${updateProperty.toUpperCase()}`);
-        // book.contacts[updateIndex].updateProperty = editedValue;
-        let foo = book.contacts[updateIndex];
-        foo["updateProperty"] = editedValue; 
 
+        //if delete by name
+        if (choice == "NAME") {
+            let deleteContact = prompt("Enter name of contact to delete.");
+            //loop through contacts, searching for name match
+            for (let i = 0; i < book.contacts.length; i++) {
+                if (book.contacts[i].name == deleteContact) {
+                    book.contacts.splice(i, 1);
+                }
+            }
+            console.log(book);
+        }
+        
+        //if update
+        if (choice == "UPDATE") {
+            let updateIndex = prompt("Enter index to update");
+            let updateProperty = prompt("Editing: NAME, EMAIL, PHONE, or RELATION?").toLowerCase();
+            
+            let foo = book.contacts[updateIndex];
+            foo[updateProperty] =  prompt(`Enter new ${updateProperty.toUpperCase()}`);
+    
+            console.log(book);
+
+        }
+
+        //if print
+        if (choice == "PRINT") {
+            console.log(book);
+
+        }
+
+        //if quit
+        if (choice == "QUIT") {
+           // break;
+        }
     }
-
-    //if print
-    if (choice == "PRINT") {
-        console.log(book);
-
-    }
-
-    //if quit
-}
